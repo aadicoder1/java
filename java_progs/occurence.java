8f7149faf58c8c31d194b081ef22b9ee14e8db57
aadicoder1 <aadityasahu1203@gmail.com>
2025-07-29 23:50:28 +0530

    .

---
+++ java_progs/occurence.java
@@ -0,0 +1,16 @@
+package java_progs;
+import java.util.*;
+
+public class occurence {
+    public static void main(String[] args) {
+        System.out.print("Enter numbers:");
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=0;i<n;i++){
+            int r=n%10;
+            
+        }
+
+        sc.close();
+    }
+}
