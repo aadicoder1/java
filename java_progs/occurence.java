c6111b836296e6e96a1eba1420b0c29f2d6ab70e
aadicoder1 <aadityasahu1203@gmail.com>
2025-07-30 00:31:40 +0530

    .

--- java_progs/occurence.java
+++ java_progs/occurence.java
@@ -6,11 +6,17 @@ public class occurence {
         System.out.print("Enter numbers:");
         Scanner sc=new Scanner(System.in);
         int n= sc.nextInt();
-        for(int i=0;i<n;i++){
+        System.out.print("Enter a no.:");
+        int i= sc.nextInt();
+        int cnt=0;
+        while(n>0){
             int r=n%10;
-            
+            if(r==i){
+                cnt++;
+            }
+            n=n/10;
         }
-
+        System.out.println(i+" occured "+cnt+" times");
         sc.close();
     }
 }
