86d7920a7789c4f48cf660766f388d65896a5495
aadicoder1 <aadityasahu1203@gmail.com>
2025-08-01 17:43:54 +0530

    linear searching

---
+++ java_progs/linearsearch.java
@@ -0,0 +1,35 @@
+package java_progs;
+import java.util.*;
+
+public class linearsearch {
+
+    public static int search(int[] arr, int n, int item) {
+        for(int i=0;i<n;i++){
+            if(arr[i]==item){
+                return i;
+            }            
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter no. of elements:");
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++){
+            System.out.print("Enter arr["+ i +"]");
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.print("Enter item to be searched:");
+        int item=sc.nextInt();
+        int result = search(arr, n, item);
+        if(result != -1) {
+            System.out.println("Item found at index: " + result);
+        } else {
+            System.out.println("Item not found.");
+        }
+
+        sc.close();
+    }
+}
