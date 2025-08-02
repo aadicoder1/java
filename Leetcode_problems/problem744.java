6d3b383e01e95de5eb469a9a572a2026596b6b45
aadicoder1 <aadityasahu1203@gmail.com>
2025-08-03 00:52:22 +0530

    744

---
+++ Leetcode_problems/problem744.java
@@ -0,0 +1,20 @@
+//find-smallest-letter-greater-than-target/
+package Leetcode_problems;
+
+public class problem744 {
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int n=letters.length;
+        for(int i=0;i<n;i++){
+            if(target<letters[i]){
+                return letters[i];
+            }
+        }
+        return letters[0];
+    }
+    public static void main(String[] args) {
+        char[] letters= {'c','f','j'};
+        char target='f';
+        System.out.println(nextGreatestLetter(letters,target));
+    }
+
+}
