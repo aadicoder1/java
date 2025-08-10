6cc8edabb630685653d82c2b7baa86249d8fe49c
aadicoder1 <aadityasahu1203@gmail.com>
2025-08-10 22:12:36 +0530

    problem 1431

---
+++ Leetcode_problems/problem1431.java
@@ -0,0 +1,30 @@
+package Leetcode_problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class problem1431 {
+    public static void main(String[] args) {
+        int[] candies={2,3,5,1,3};
+        int extraCandies=3;
+        System.out.println(kidsWithCandies(candies,extraCandies));
+    }
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max=-1;
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]>max){
+                max=candies[i];
+            }
+        }
+
+        List<Boolean> bool_arr = new ArrayList();
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]+extraCandies>=max){
+                bool_arr.add(true);
+            } else {
+                bool_arr.add(false);
+            }
+        }
+        return bool_arr;
+    }
+}
--- java_progs/sorting/selection.java
+++ java_progs/sorting/selection.java
@@ -1,5 +1,11 @@
 package java_progs.sorting;
 
+
 public class selection {
-    
+    public static void main(String[] args) {
+        
+    }
+    public static int[] main(int[] arr) {
+        return arr;
+    }
 }
