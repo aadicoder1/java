e3908fa1064f4cba156d4dadc18b83c1631f5573
aadicoder1 <aadityasahu1203@gmail.com>
2025-08-11 23:37:52 +0530

    selection sort

--- java_progs/sorting/selection.java
+++ java_progs/sorting/selection.java
@@ -1,11 +1,32 @@
 package java_progs.sorting;
 
+import java.util.Arrays;
 
 public class selection {
     public static void main(String[] args) {
-        
+        int[] arr={3,1,5,4,2};
+        selectionsort(arr);
+        System.out.println(Arrays.toString(arr));
     }
-    public static int[] main(int[] arr) {
-        return arr;
+    public static void selectionsort(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            int last=arr.length-i-1;
+            int max=getmax(arr,0,last);
+            swap(arr, max , last);
+        }
+    }
+    public static void swap(int[] arr, int first , int last){
+        int temp=arr[first];
+        arr[first]=arr[last];
+        arr[last]=temp;
+    }
+    public static int getmax(int[] arr,int start,int last){
+        int max=0;
+        for(int i=start;i<=last;i++){
+            if(arr[max]<arr[i]){
+                max=i;
+            }
+        }
+        return max;
     }
 }
