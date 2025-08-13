041461421cdf0e3cab7021e61dcac053319e3b4b
aadicoder1 <aadityasahu1203@gmail.com>
2025-08-14 00:09:56 +0530

    problem442

---
+++ Leetcode_problems/not_submitted/problem442.java
@@ -0,0 +1,72 @@
+//find-all-duplicates-in-an-array/
+package Leetcode_problems.not_submitted;
+
+import java.util.*;
+
+//for small constraints , use hashset for optimization 
+/*public class problem442 {
+    public static void main(String[] args) {
+        int[] nums={1,2,2,2};
+        System.out.println(findDuplicates(nums));
+    }
+    public static List<Integer> findDuplicates(int[] nums) {
+        cyclicsort(nums);
+        List<Integer> dups = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if (nums[i] != i+1) {
+                if (!dups.contains(nums[i])) {
+                    dups.add(nums[i]);}
+                } 
+        }
+        Collections.sort(dups);
+        return dups;
+    }
+    public static int[] cyclicsort(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int correct=nums[i]-1;
+            if (nums[correct] != nums[i]){
+                int temp=nums[correct];
+                nums[correct]=nums[i];
+                nums[i]=temp;
+            } else {
+                i++;
+            }
+        }
+        return nums;
+    }
+}*/
+
+
+
+
+
+public class problem442 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2, 2};
+        System.out.println(findDuplicates(nums));
+    }
+
+    public static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> dups = new ArrayList<>();
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                // swap
+                int temp = nums[correct];
+                nums[correct] = nums[i];
+                nums[i] = temp;
+            } else {
+                if (i != correct) {
+                    // duplicate found (avoid adding same number twice)
+                    if (dups.isEmpty() || dups.get(dups.size() - 1) != nums[i]) {
+                        dups.add(nums[i]);
+                    }
+                }
+                i++;
+            }
+        }
+        return dups;
+    }
+}
