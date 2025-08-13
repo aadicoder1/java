28f662bb7b64058ab32f4b512f8ac63948cf4c3d
aadicoder1 <aadityasahu1203@gmail.com>
2025-08-14 01:45:48 +0530

    problem41

---
+++ Leetcode_problems/not_submitted/problem41.java
@@ -0,0 +1,32 @@
+//first-missing-positive/
+package Leetcode_problems.not_submitted;
+
+public class problem41 {
+    public static void main(String[] args) {
+        int[] nums={7,8,9,11,12};
+        System.out.println(firstMissingPositive(nums));
+    }
+    public static int firstMissingPositive(int[] nums) {
+        cyclicsort(nums);
+        for (int i=0;i<nums.length;i++){
+            if(nums[i] != i+1){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+    public static int cyclicsort(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+                int correct=nums[i]-1;
+                if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]){
+                    int temp=nums[correct];
+                    nums[correct]=nums[i];
+                    nums[i]=temp;
+                } else {
+                    i++;
+                }            
+        }
+        return -1;
+    }
+}
