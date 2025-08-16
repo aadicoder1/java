03ccef4c8075f57b0a7a0f9aa9ac2e48967fe5c9
aadicoder1 <aadityasahu1203@gmail.com>
2025-08-16 11:59:57 +0530

    problem1450

--- Leetcode_problems/not_submitted/problem1450.java
+++ Leetcode_problems/not_submitted/problem1450.java
@@ -2,9 +2,20 @@
 package Leetcode_problems.not_submitted;
 
 public class problem1450 {
-    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
-        for (int i=0;i<startTime){
-
+    public static void main(String[] args) {
+        int[] startTime = {4};
+        int[] endTime = {4};
+        int queryTime = 4;
+        System.out.println();
+        System.out.println(busyStudent(startTime, endTime, queryTime));
+    }
+    public static int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int cnt=0;
+        for (int i=0;i<startTime.length;i++){
+            if (startTime[i]<=queryTime && endTime[i]>=queryTime) {
+                cnt++;
+            }
         }
+        return cnt;
     }
 }
