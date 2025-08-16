bfc20a650983c82ff9894322821230d8239281bc
aadicoder1 <aadityasahu1203@gmail.com>
2025-08-16 11:45:32 +0530

    .

---
+++ Leetcode_problems/not_submitted/problem1450.java
@@ -0,0 +1,10 @@
+//number-of-students-doing-homework-at-a-given-time/
+package Leetcode_problems.not_submitted;
+
+public class problem1450 {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        for (int i=0;i<startTime){
+
+        }
+    }
+}
--- Leetcode_problems/submitted/problem645.java
+++ Leetcode_problems/submitted/problem645.java
@@ -1,5 +1,5 @@
 //set-mismatch/
-package Leetcode_problems.not_submitted;
+package Leetcode_problems.submitted;
 
 import java.util.*;
 
