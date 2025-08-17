73e63cd2a523b3ed4b98f7019158b8cf24ce40db
aadicoder1 <aadityasahu1203@gmail.com>
2025-08-17 12:52:20 +0530

    problem3127

--- Leetcode_problems/not_submitted/problem3127.java
+++ Leetcode_problems/not_submitted/problem3127.java
@@ -1,5 +1,26 @@
+//make-a-square-with-the-same-color/
 package Leetcode_problems.not_submitted;
 
 public class problem3127 {
-    
+    public static void main(String[] args) {
+        char[][] grid={{'W','B'},
+    {'B','B'}
+};
+        System.out.println(canMakeSquare(grid));
+    }
+    public static boolean canMakeSquare(char[][] grid) {
+        if (grid[0][0]==grid[0][1] && grid[1][0]==grid[0][1] || grid[1][0]==grid[1][1] && grid[0][0]==grid[1][1]|| grid[0][0]==grid[0][1] && grid[0][0]==grid[1][1]) {
+            return true;
+        }
+        else if (grid[0][1]==grid[0][2] && grid[1][1]==grid[0][2] || grid[1][1]==grid[1][2] && grid[0][1]==grid[1][2]|| grid[0][1]==grid[0][2] && grid[0][1]==grid[1][2]) {
+            return true;
+        }
+        else if (grid[1][0]==grid[1][1] && grid[2][0]==grid[1][1] || grid[2][0]==grid[2][1] && grid[1][0]==grid[2][1]|| grid[1][0]==grid[1][1] && grid[1][0]==grid[2][1]) {
+            return true;
+        }
+        else if (grid[1][1]==grid[1][2] && grid[2][1]==grid[1][2] || grid[2][1]==grid[2][2] && grid[1][1]==grid[2][2]|| grid[1][1]==grid[1][2] && grid[1][1]==grid[2][2]) {
+            return true;
+        }
+        return false;
+    }
 }
