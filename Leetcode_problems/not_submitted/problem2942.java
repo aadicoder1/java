9693055c4bc6ce09737062f6f4f25f9d253c35cd
aadicoder1 <aadityasahu1203@gmail.com>
2025-08-17 18:26:29 +0530

    problem2942

---
+++ Leetcode_problems/not_submitted/problem2942.java
@@ -0,0 +1,25 @@
+//find-words-containing-character/
+package Leetcode_problems.not_submitted;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class problem2942 {
+    public static void main(String[] args) {
+        String[] words = {"abc","bcd","aaaa","cbc"};
+        char x = 'a';
+        System.out.println(findWordsContaining(words,x));
+    }
+    public static List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> ind= new ArrayList<Integer>() ;
+        for(int i=0;i<words.length;i++){
+            for(int j=0;j<words[i].length();j++){
+                if(words[i].charAt(j)==x){
+                    ind.add(i);
+                    break;
+                }
+            }
+        }
+        return ind ;
+    }
+}
