034efc9d06c082b6dbbe25520b4d6899556bbf5f
aadicoder1 <aadityasahu1203@gmail.com>
2025-08-18 17:33:51 +0530

    problem1331

---
+++ Leetcode_problems/not_submitted/problem1331.java
@@ -0,0 +1,34 @@
+//rank-transform-of-an-array/
+package Leetcode_problems.not_submitted;
+
+import java.util.Arrays;
+
+public class problem1331 {
+    public static void main(String[] args) {
+        int[] arr = {40, 10, 20, 20, 30};
+        System.out.println(Arrays.toString(arrayRankTransform(arr)));
+    }
+
+public static int[] arrayRankTransform(int[] arr) {
+        if (arr.length == 0) return new int[0];
+
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        int[] ranks = new int[sorted.length];
+        int rank = 1;
+        ranks[0] = rank;
+        for (int i = 1; i < sorted.length; i++) {
+            if (sorted[i] != sorted[i - 1]) {
+                rank++;
+            }
+            ranks[i] = rank;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int idx = Arrays.binarySearch(sorted, arr[i]);
+            arr[i] = ranks[idx];
+        }
+        return arr;
+    }
+}
