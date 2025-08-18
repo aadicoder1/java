f9aba52e3ceea53aa1eb33b5f98d331eff82c0ab
aadicoder1 <aadityasahu1203@gmail.com>
2025-08-18 23:47:09 +0530

    .

--- java_progs/recursion/binary_search.java
+++ java_progs/recursion/binary_search.java
@@ -1,5 +1,26 @@
 package java_progs.recursion;
+import java.util.*;
 
 public class binary_search {
-    
+    public static void main(String[] args) {
+        int[] nums={3,6,2,1,8};
+        Arrays.sort(nums);
+        int target=6;
+        int s=0 , e=nums.length-1;
+        System.out.println(BS(nums,target ,s,e));
+    }
+    public static int BS(int[] nums ,int target, int s ,int e) {
+        if(s > e){
+            return -1;
+        }
+        int m = s + (e - s) / 2;
+        if(nums[m] == target){
+            return m;
+        }
+        if(nums[m] > target){
+            return BS(nums, target, s, m - 1);
+        } else {
+            return BS(nums, target, m + 1, e);
+        }
+    }
 }
