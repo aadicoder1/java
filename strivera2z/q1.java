29bff88503d7a87be84dfa1b11ccd4becbf7bf13
aadicoder1 <aadityasahu1203@gmail.com>
2025-11-05 19:22:22 +0530

    .

---
+++ strivera2z/q1.java
@@ -0,0 +1,12 @@
+package strivera2z;
+import java.util.* ;
+
+public class q1 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int dig= sc.nextInt();
+        System.out.println(dig);
+
+        sc.close();
+    }
+}
