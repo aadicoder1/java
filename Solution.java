a765480ed110514f91fbd4753d06250dfe677edc
aadicoder1 <aadityasahu1203@gmail.com>
2026-01-25 13:11:36 +0530

    .

--- Solution.java
+++ Solution.java
@@ -1,145 +1,53 @@
 import java.util.*;
-/*
-//import java.io.*;
 
-class Solution{
-    public static int power(int n){
-        int pow=1;
-        for(int i=0;i<n;i++){
-            pow=pow*2;
-        }
-        return pow;
-    }
+public class Solution {
+
     
-    public static void main(String []argh){
-        Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
-            
-            int sum=0;
-            for(int j=0;j<n;j++){
-                int p = power(j);
-                sum=sum+p;
-                System.out.println(a+b*sum);
-            }
-            System.out.println();
-        }
-        in.close();
-    }
-}*/
+    
+    /*public static void main(String[] args) {
+        System.out.println("*****\n   *\n  *\n *\n*****");
+    }*/
 
 
 
-/*
- class Solution{
-    public static void main(String []args){
-        Scanner sc = new Scanner(System.in);
+        //is prime number??
+    /*public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter no.:");
         int n=sc.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(" * " );
+        int t=0;
+        for (int i=2;i<n/2+1;i++){
+            if(n%i==0){
+                t++;
+                break;
             }
-            System.out.println("  " );
+        }
+        if(t==0){
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
         }
         sc.close();
+    }*/
+
+
+
+    //print all prime no. till n
+
+    public static void main(String[] args) {
+        
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
- */
 
-
-/*class Solution{
-    public static void main(String []args){
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        pattern(n);
-        sc.close();
-    }
-    public static void pattern(int n){
-        for(int i=n;i>=0;i--){
-            for(int j=i;j<n;j++){
-                System.out.print(" * " );
-            }
-            System.out.println("  " );
-        }
-
-    }
-}*/
-
-
-
-/*class Solution{
-    public static void main(String []args){
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        pattern(n);
-        sc.close();
-    }
-    public static void pattern(int n){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(i+j-1>=n){
-                System.out.print("*  ");}
-                else{
-                    System.out.print("   ");
-                }
-            }
-            System.out.println();
-        } 
-    }
-}  */ 
-
-
-
-/*
-class Solution{
-    public static void main(String []args){
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        pattern(n);
-        sc.close();
-    }
-    public static void pattern(int n){
-        char ch='a';
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(i+j-1>=n){
-                    System.out.print(ch+"  ");
-                    ch++;
-                }
-                else{
-                    System.out.print("   ");
-                }
-            }
-            System.out.println();
-        } 
-    }
-}*/
-
-
-
-
-
-class Solution {
-    public int[] intersection(int[] nums1,int[] nums2){
-        HashSet<Integer> set1=new HashSet<>();
-        HashSet<Integer> result=new HashSet<>();
-
-        for(int n:nums1){
-            set1.add(n);
-        }
-        for(int n:nums2){
-            if(set1.contains(n)){
-                result.add(n);
-            }
-        }
-        int[] ans = new int[result.size()];
-        int i = 0;
-        for (int x:result){
-            ans[i++] = x;
-        }
-        return ans;
-    }
-}
