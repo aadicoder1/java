369c98b458f26e8c04b055c5c596a55d1a257923
aadicoder1 <aadityasahu1203@gmail.com>
2026-01-27 00:03:08 +0530

    .

--- Solution.java
+++ Solution.java
@@ -39,19 +39,19 @@ public class Solution {
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
 
-        for (int i=1;i<=n;i++){
+        for (int i=2;i<=n;i++){
             int cnt=0;
-            for(int j=2;j*j<=n;j++){
+            for(int j=2;j*j<=i;j++){
                 if(i%j==0){
                     cnt++;
                     break;
                 }
             }
 
-            if (cnt==0 || i==2){
+            if (cnt==0){
                 System.out.println(i);
             }
-            
+
         }
         sc.close();
     }
